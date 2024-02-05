a71ec7533a3ef755f6296bb6e050c00b3e23b473
Cephas Tokoli <matumasie@gmail.com>
2024-02-05 23:42:46 +0000

    Update CreateFormBtn.tsx

--- components/CreateFormBtn.tsx
+++ components/CreateFormBtn.tsx
@@ -1,57 +1,102 @@
-"use client"
+/* eslint-disable @typescript-eslint/no-misused-promises */
+'use client'
 
 import React from 'react'
-import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription  } from './ui/dialog'
-import { Button } from "./ui/button";
-import { BsFileEarmarkPlus } from "react-icons/bs";
-import { ImSpinner2 } from "react-icons/im";
-import { Label } from "./ui/label";
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from './ui/dialog'
+import { Button } from './ui/button'
+import { ImSpinner2 } from 'react-icons/im'
 import {
-    Form,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "./ui/form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import { useForm } from 'react-hook-form';
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from './ui/form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Input } from './ui/input'
+import { Textarea } from './ui/textarea'
+import { toast } from './ui/use-toast'
+import { formSchema, type formSchemaType } from '@/schemas/form'
+import { CreateForm } from '@/actions/form'
 
-const formSchema = z.object({
-    name: z.string().min(4, "Name must be at least 4 characters"),
-    description: z.string().optional(),
-})
+export default function CreateFormBtn (): React.JSX.Element {
+  const form = useForm<formSchemaType>({
+    resolver: zodResolver(formSchema)
+  })
 
-type formSchemaType = z.infer<typeof formSchema>; 
-
-export default function CreateFormBtn() {
-    const form = useForm<formSchemaType>({
-        resolver: zodResolver(formSchema),
-    });
-
-    function handleSubmit(values: formSchemaType) {
-        console.log(values);
+  async function onSubmit (values: formSchemaType): Promise<void> {
+    try {
+      const formID = await CreateForm(values)
+      toast({
+        title: 'Success',
+        description: 'Form created successfully',
+        variant: 'default'
+      })
+      console.log(formID)
+    } catch (error) {
+      toast({
+        title: 'Error',
+        description: `Something went wrong, please try again later. ${(error as Error).toString()}`,
+        variant: 'destructive'
+      })
+      console.log(error)
     }
-    return (
-        <Dialog>
-            <DialogTrigger asChild>
-             <Button> Create New Form</Button>
-            </DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Create form</DialogTitle>
-                    <DialogDescription>
-                        Create a new form to start collecting responses
-                    </DialogDescription>
-                </DialogHeader>
-                <Form {...form} >
-                    <form onSubmit={form.handleSubmit(handleSubmit)} >
+  }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button> Create New Form</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create form</DialogTitle>
+          <DialogDescription>
+            Create a new form to start collecting responses
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form} >
+          <form onSubmit={() => form.handleSubmit(onSubmit)} className="space-y-2">
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )} />
 
-                    </form>
-                </Form>
-            </DialogContent>
-        </Dialog>
-    )
+            <FormField
+              control={form.control}
+              name='description'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea rows={5} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )} />
+          </form>
+        </Form>
+        <DialogFooter>
+          <Button
+            onClick={form.handleSubmit(onSubmit)}
+            disabled={form.formState.isSubmitting}
+            className='w-full mt-4'>
+            {!form.formState.isSubmitting && <span>Save</span>}
+            {form.formState.isSubmitting && (
+              <ImSpinner2 className='animate-spin' />
+            )}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
 }
- 
