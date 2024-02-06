61a5ba3e196244617665cf3813e0c18c8c9e164d
Cephas Tokoli <matumasie@gmail.com>
2024-02-06 23:36:18 +0000

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,12 +3,12 @@ import ThemeSwitcher from '@/components/ThemeSwitcher'
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
+export default function Layout ({ children }: { children: React.ReactNode }): React.JSX.Element {
+  return (
         <div className='flex flex-col min-h-screen min-w-full bg-background max-h-screen'>
             <nav className='flex justify-between items-center border-b border-border h-[60px] px-4 py-2'>
                 <Logo />
-                <div  className='flex gap-4 items-center'>
+                <div className='flex gap-4 items-center'>
                     <ThemeSwitcher />
                     <UserButton afterSignOutUrl='/sign-in' />
                 </div>
@@ -17,5 +17,5 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 {children}
             </main>
         </div>
-    )
+  )
 }
