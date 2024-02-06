2983a409dedf623151304951d4812a02b0f6005e
Cephas Tokoli <matumasie@gmail.com>
2024-02-06 23:36:18 +0000

    Update page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,48 +1,49 @@
-import { GetFormStats } from "@/actions/form";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Skeleton } from "@/components/ui/skeleton";
-import { ReactNode, Suspense } from "react";
-import { LuView } from "react-icons/lu";
-import { FaWpforms } from "react-icons/fa";
-import { HiCursorClick } from "react-icons/hi";
-import { TbArrowBounce } from "react-icons/tb"
-import { Separator } from "@/components/ui/separator";
-import CreateFormBtn from "@/components/CreateFormBtn";
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { GetFormStats } from '@/actions/form'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
+import React, { type ReactNode, Suspense } from 'react'
+import { LuView } from 'react-icons/lu'
+import { FaWpforms } from 'react-icons/fa'
+import { HiCursorClick } from 'react-icons/hi'
+import { TbArrowBounce } from 'react-icons/tb'
+import { Separator } from '@/components/ui/separator'
+import CreateFormBtn from '@/components/CreateFormBtn'
 
-
-export default function Home() {
+export default function Home (): React.JSX.Element {
   return (
     <div className="container pt-4">
       <Suspense fallback={<StatsCards loading={true} />}>
         <CardStatsWrapper />
-      </Suspense> 
+      </Suspense>
       <Separator className="my-6" />
       <h2 className="text-3xl font-bold col-span-2">Your Forms</h2>
       <Separator className="my-6" />
-      <CreateFormBtn /> 
+      <CreateFormBtn />
     </div>
-  );
+  )
 }
 
-async function CardStatsWrapper() {
-  const stats = await GetFormStats();
-  return <StatsCards loading={false} data={stats} />;
+async function CardStatsWrapper (): Promise<React.JSX.Element> {
+  const stats = await GetFormStats()
+  return <StatsCards loading={false} data={stats} />
 }
 
 interface StatsCardProps {
-  data?: Awaited<ReturnType<typeof GetFormStats>>;
-  loading: boolean;
+  data?: Awaited<ReturnType<typeof GetFormStats>>
+  loading: boolean
 }
 
-function StatsCards(props: StatsCardProps) {
-  const { data, loading } = props;
+function StatsCards (props: StatsCardProps): React.JSX.Element {
+  const { data, loading } = props
 
   return <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
     <StatsCard
       title="Total visits"
       icon={<LuView className="text-blue-600" />}
       helperText="All time form visits"
-      value={data?.visits.toLocaleString() || ""}
+      value={data?.visits.toLocaleString() || ''}
       loading={loading}
       className="shadow-md shadow-blue-600"
     />
@@ -50,7 +51,7 @@ function StatsCards(props: StatsCardProps) {
       title="Total Submissions"
       icon={<FaWpforms className="text-yellow-600" />}
       helperText="All time form submissions"
-      value={data?.submissions.toLocaleString() || ""}
+      value={data?.submissions.toLocaleString() || ''}
       loading={loading}
       className="shadow-md shadow-yellow-600"
     />
@@ -58,7 +59,7 @@ function StatsCards(props: StatsCardProps) {
       title="Submission Rate"
       icon={<HiCursorClick className="text-green-600" />}
       helperText="Visits that result in form submission"
-      value={data?.submissionRate.toLocaleString() + "%" || ""}
+      value={data?.submissionRate.toLocaleString() + '%' || ''}
       loading={loading}
       className="shadow-md shadow-green-600"
     />
@@ -66,27 +67,28 @@ function StatsCards(props: StatsCardProps) {
       title="Bounce Rate"
       icon={<TbArrowBounce className="text-red-600" />}
       helperText="Visitors that leave without interacting"
-      value={data?.submissionRate.toLocaleString() + "%" || ""}
+      value={data?.submissionRate.toLocaleString() + '%' || ''}
       loading={loading}
       className="shadow-md shadow-red-600"
     />
   </div>
 }
 
-function StatsCard({
+function StatsCard ({
   title,
   icon,
   helperText,
   value,
   loading,
-  className }: {
-    title: string;
-    icon: ReactNode;
-    helperText: string;
-    value: string;
-    className: string;
-    loading: boolean;
-  }) {
+  className
+}: {
+  title: string
+  icon: ReactNode
+  helperText: string
+  value: string
+  className: string
+  loading: boolean
+}): React.JSX.Element {
   return (
     <Card className={className}>
       <CardHeader className="flex flex-row items-center justify-between pb-2">
@@ -106,5 +108,4 @@ function StatsCard({
       </CardContent>
     </Card>
   )
-
 }
