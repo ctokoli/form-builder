ae28dac2959884e1a21e8d59b3ad612a64933e59
Cephas Tokoli <matumasie@gmail.com>
2024-02-08 19:03:40 +0000

    Update page.tsx

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
 
- 
-export default function Page() {
-  return <SignUp />;
+export default function Page (): React.JSX.Element {
+  return <SignUp />
 }
