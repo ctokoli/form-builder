d1e8e2224d381d2e6a6b623d712f6f67c2eae43e
Cephas Tokoli <matumasie@gmail.com>
2024-02-09 22:17:32 +0000

    Update ThemeSwitcher.tsx

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,31 +1,31 @@
 'use client'
 
-import { SunIcon, MoonIcon, DesktopIcon } from '@radix-ui/react-icons';
-import { Tabs, TabsList, TabsTrigger } from '@radix-ui/react-tabs';
+import { SunIcon, MoonIcon, DesktopIcon } from '@radix-ui/react-icons'
+import { Tabs, TabsList, TabsTrigger } from '@radix-ui/react-tabs'
 import { useTheme } from 'next-themes'
 import React, { useEffect } from 'react'
 
-export default function ThemeSwitcher() {
-    const { theme, setTheme } = useTheme();
-    const [mounted, setMounted] = React.useState(false);
+export default function ThemeSwitcher (): React.JSX.Element | null {
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = React.useState(false)
 
-    useEffect(() => setMounted(true), []);
-    if (!mounted) return null;
-    return (
+  useEffect(() => { setMounted(true) }, [])
+  if (!mounted) return null
+  return (
         <div>
             <Tabs defaultValue={theme}>
-                <TabsList className='border'>
-                    <TabsTrigger value='light' onClick={() => setTheme("light")}>
+                <TabsList className='[&>*]:p-2'>
+                    <TabsTrigger value='light' onClick={() => { setTheme('light') }}>
                         <SunIcon className='h-[1.2rem] w-[1.2rem] ' />
                     </TabsTrigger>
-                    <TabsTrigger value='dark' onClick={() => setTheme("dark")}>
+                    <TabsTrigger value='dark' onClick={() => { setTheme('dark') }}>
                         <MoonIcon className='h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0' />
                     </TabsTrigger>
-                    <TabsTrigger value='light' onClick={() => setTheme("system")}>
+                    <TabsTrigger value='light' onClick={() => { setTheme('system') }}>
                         <DesktopIcon className='h-[1.2rem] w-[1.2rem]' />
                     </TabsTrigger>
                 </TabsList>
             </Tabs>
         </div>
-    )
+  )
 }
