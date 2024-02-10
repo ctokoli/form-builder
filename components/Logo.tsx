57cd6ef93c945bef9e1b2a635c2f399d0929bd09
Cephas Tokoli <matumasie@gmail.com>
2024-02-10 20:23:44 +0000

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Logo (): JSX.Element {
   return (
-    <Link href={'/'} className='font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer'>
+    <Link href={'/'} className='font-bold text-lg md:text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer'>
         PageForm Builder
     </Link>
   )
