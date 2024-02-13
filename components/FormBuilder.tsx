64831bb54ef25032d0e242e7430d2e80735bb621
Cephas Tokoli <matumasie@gmail.com>
2024-02-13 21:25:26 +0000

    Update FormBuilder.tsx

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -7,6 +7,7 @@ import SaveFormBtn from './SaveFormBtn'
 import PublishFormBtn from './PublishFormBtn'
 import Designer from './Designer'
 import { DndContext } from '@dnd-kit/core'
+import DragOverlayWrapper from './DragOverlayWrapper'
 
 const FormBuilder = ({ form }: { form: Form }): JSX.Element => {
   return (
@@ -33,6 +34,7 @@ const FormBuilder = ({ form }: { form: Form }): JSX.Element => {
                     <Designer />
                 </div>
             </main>
+            <DragOverlayWrapper />
         </DndContext>
   )
 }
