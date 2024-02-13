1076de5cf052b50dc669cf738033136cf9de33e5
Cephas Tokoli <matumasie@gmail.com>
2024-02-13 21:25:26 +0000

    Update SidebarBtnElement.tsx

--- components/SidebarBtnElement.tsx
+++ components/SidebarBtnElement.tsx
@@ -26,3 +26,20 @@ export default function SidebarBtnElement ({ formElement }: { formElement: FormE
         </Button>
   )
 }
+
+export function SidebarBtnElementDragOverlay ({
+  formElement
+}: {
+  formElement: FormElement
+}): JSX.Element {
+  const { label, icon: Icon } = formElement.designerBtnElement
+  return (
+      <Button
+          variant={'outline'}
+          className='flex flex-col gap-2 h-[120px] w-[120px] cursor-grab'
+         >
+          <Icon className='h-8 w-8 text-primary cursor-grab' />
+          <p className='text-xs'>{label}</p>
+          </Button>
+  )
+}
