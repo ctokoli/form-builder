4f7affe4ef3772d36a82e2fcb3db0517d2bd3a35
Cephas Tokoli <matumasie@gmail.com>
2024-02-19 22:34:11 +0000

    Add DesignerContext.tsx

---
+++ components/context/DesignerContext.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import React, { type ReactNode, createContext, useState } from 'react'
+import { type FormElementInstance } from '../FormElements'
+
+interface DesignerContextType {
+  elements: FormElementInstance[]
+  addElement: (index: number, element: FormElementInstance) => void
+}
+
+export const DesignerContext = createContext<DesignerContextType | null>(null)
+
+export default function DesignerContextProvider ({
+  children
+}: { children: ReactNode }): JSX.Element {
+  const [elements, setElements] = useState<FormElementInstance[]>([])
+  const addElement = (index: number, element: FormElementInstance): void => {
+    setElements((prev) => {
+      const newElements = [...prev]
+      newElements.splice(index, 0, element)
+      return newElements
+    })
+  }
+
+  return <DesignerContext.Provider value={
+    {
+      elements,
+      addElement
+    }
+  }>{children}</DesignerContext.Provider>
+}
