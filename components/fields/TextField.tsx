9ada8808a23cc86b66df76c7716688935eff2862
Cephas Tokoli <matumasie@gmail.com>
2024-02-24 21:35:15 +0000

    Update TextField.tsx

--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -2,29 +2,36 @@
 
 import React from 'react'
 
-import { type FormElement, type ElementsType } from '../FormElements'
+import { type FormElement, type ElementsType, type FormElementInstance } from '../FormElements'
 import { MdTextFields } from 'react-icons/md'
+import DesignerComponent from './DesignerComponent'
 
 const type: ElementsType = 'TextField'
+
+const extraAttributes = {
+  label: 'Text field',
+  helperText: 'Helper Text',
+  required: false,
+  placeholder: 'Input Value here...'
+}
 
 export const TextFieldFormElement: FormElement = {
   type,
   construct: (id: string) => ({
     id,
     type,
-    extraAttributes: {
-      label: 'Text field',
-      helperText: 'Helper Text',
-      required: false,
-      placeholder: 'Input Value here...'
-    }
+    extraAttributes
   }),
   designerBtnElement: {
     icon: MdTextFields,
     label: 'Text Field'
   },
-  designerComponent: () => <div> Designer Component</div>,
+  designerComponent: DesignerComponent,
   formComponent: () => <div>form Component</div>,
   propertiesComponent: () => <div>Properties Component</div>
 
+}
+
+export type CustomInstance = FormElementInstance & {
+  extraAttributes: typeof extraAttributes
 }
