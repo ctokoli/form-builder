68a548f11df1f9e3347f753f913d914f9f468696
Cephas Tokoli <matumasie@gmail.com>
2024-03-11 10:40:09 +0000

    Update DesignerElementWrapper.tsx

--- components/DesignerElementWrapper.tsx
+++ components/DesignerElementWrapper.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 import { type FormElementInstance, FormElements } from './FormElements'
-import { useDroppable } from '@dnd-kit/core'
+import { useDraggable, useDroppable } from '@dnd-kit/core'
 import { Button } from './ui/button'
 import { BiSolidTrash } from 'react-icons/bi'
 import useDesigner from './hooks/useDesigner'
 import { cn } from '@/lib/utils'
 
 export default function DesignerElementWrapper ({ element }: { element: FormElementInstance }): JSX.Element {
-  const { removeElement } = useDesigner()
+  const { removeElement, setSelectedElement, selectedElement } = useDesigner()
   const [mouseIsOver, setMouseIsOver] = useState<boolean>(false)
 
   const topHalf = useDroppable({
@@ -27,9 +27,23 @@ export default function DesignerElementWrapper ({ element }: { element: FormElem
       isBottomHalfDesignerElement: true
     }
   })
+
+  const draggable = useDraggable({
+    id: element.id + '-drag-handler',
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isDesignerElement: true
+    }
+  })
+
+  if (draggable === null) return null
   const DesignerElement = FormElements[element.type].designerComponent
   return (
     <div
+      ref={draggable.setNodeRef}
+      {...draggable.listeners}
+      {...draggable.attributes}
       className="relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset"
       onMouseEnter={() => {
         setMouseIsOver(true)
@@ -37,24 +51,19 @@ export default function DesignerElementWrapper ({ element }: { element: FormElem
       onMouseLeave={() => {
         setMouseIsOver(false)
       }}
-    >
-      <div
-        className='flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 pointer-events-none'
-      >
-        <DesignerElement elementInstance={element} />
-      </div>
+     >
       <div
         ref={topHalf.setNodeRef}
-        className="absolute w-full bottom-0 h-1/2 rounded-md"
+        className="absolute w-full h-1/2 rounded-md"
       />
       <div
         ref={bottomHalf.setNodeRef}
-        className="absolute w-full bottom-0 h-1/2 rounded-md"
+        className="absolute w-full bottom-0 h-1/2 rounded-b-md"
       />
       {mouseIsOver && (
         <>
           <div
-            className="absolute right-0 h-full">
+            className="absolute right-0 h-full z-100">
             <Button
               className='flex justify-items-center h-full border rounded-md rounded-l-none bg-red-500'
               variant={'outline'}
@@ -71,6 +80,19 @@ export default function DesignerElementWrapper ({ element }: { element: FormElem
             </p>
           </div>
         </>
+      )}
+      {topHalf.isOver && (
+        <div className="absolute top-0 w-full rounded-md h-[7px] bg-primary rounded-b-none"></div>
+      )}
+       <div
+        className={cn('flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 pointer-events-none',
+          mouseIsOver && 'opacity-30'
+        )}
+      >
+        <DesignerElement elementInstance={element} />
+      </div>
+      {bottomHalf.isOver && (
+        <div className="absolute bottom-0 w-full rounded-b-md h-[7px] bg-primary "></div>
       )}
     </div>
   )
