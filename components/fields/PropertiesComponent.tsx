06a0500671015e949f886d600c04eb2c36aeac21
Cephas Tokoli <matumasie@gmail.com>
2024-03-19 22:52:42 +0000

    Update PropertiesComponent.tsx

--- components/fields/PropertiesComponent.tsx
+++ components/fields/PropertiesComponent.tsx
@@ -1,10 +1,30 @@
+import { useForm } from 'react-hook-form'
 import { type FormElementInstance } from '../FormElements'
-import { type CustomInstance } from './TextField'
+import { propertiesSchema, type CustomInstance } from './TextField'
+import { type z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect } from 'react'
+
+type propertiesFormSchemaType = z.infer<typeof propertiesSchema>
 
 export default function PropertiesComponent ({ elementInstance }: {
   elementInstance: FormElementInstance
 }): JSX.Element {
   const element = elementInstance as CustomInstance
+  const form = useForm<propertiesFormSchemaType>({
+    resolver: zodResolver(propertiesSchema),
+    mode: 'onBlur',
+    defaultValues: {
+      label: element.extraAttributes.label,
+      helperText: element.extraAttributes.helperText,
+      required: element.extraAttributes.required,
+      placeHolder: element.extraAttributes.placeholder
+    }
+  })
+
+  useEffect(() => {
+    form.reset(element.extraAttributes)
+  }, [element, form])
   return (
   <div className="">Form property for { element.extraAttributes.label}</div>
   )
