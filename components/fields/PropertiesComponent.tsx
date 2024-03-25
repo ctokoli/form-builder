14177cb2bd258f3f67cd4951c50e4ef10ac21552
Cephas Tokoli <matumasie@gmail.com>
2024-03-25 17:49:47 +0000

    Update PropertiesComponent.tsx

--- components/fields/PropertiesComponent.tsx
+++ components/fields/PropertiesComponent.tsx
@@ -1,9 +1,12 @@
-import { useForm } from 'react-hook-form'
+import React, { useForm } from 'react-hook-form'
 import { type FormElementInstance } from '../FormElements'
 import { propertiesSchema, type CustomInstance } from './TextField'
 import { type z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useEffect } from 'react'
+import useDesigner from '../hooks/useDesigner'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
+import { Input } from '../ui/input'
 
 type propertiesFormSchemaType = z.infer<typeof propertiesSchema>
 
@@ -11,6 +14,7 @@ export default function PropertiesComponent ({ elementInstance }: {
   elementInstance: FormElementInstance
 }): JSX.Element {
   const element = elementInstance as CustomInstance
+  const { updateElement } = useDesigner()
   const form = useForm<propertiesFormSchemaType>({
     resolver: zodResolver(propertiesSchema),
     mode: 'onBlur',
@@ -25,7 +29,43 @@ export default function PropertiesComponent ({ elementInstance }: {
   useEffect(() => {
     form.reset(element.extraAttributes)
   }, [element, form])
-  return (
-  <div className="">Form property for { element.extraAttributes.label}</div>
-  )
+
+  function applyChanges (values: propertiesFormSchemaType): void {
+    const { label, helperText, placeHolder, required } = values
+    updateElement(element.id, {
+      ...element,
+      extraAttributes: {
+        label,
+        helperText,
+        placeHolder,
+        required
+      }
+    })
+    console.log('blur')
+  }
+  return <Form {...form}>
+    <form onBlur={() => { form.handleSubmit(applyChanges) }}
+    className='space-y-3'
+    // onSubmit={(e) => {
+    //   e.preventDefault()
+    // }}
+    >
+      <FormField
+        control={form.control}
+        name='label'
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Label</FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+            <FormDescription>
+              The label of the field. <br /> it will be displayed above the field
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+     />
+    </form>
+  </Form>
 }
